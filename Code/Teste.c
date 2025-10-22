2cccf527b70e31dd11d80a70f5045a3f51e83a72
Kauã V. <nokknee10@gmail.com>
2025-10-22 14:16:08 -0300

    aaaaa

--- Code/Teste.c
+++ Code/Teste.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    int codigo;
+    char nome[20];
+    float altura;
+    float peso;
+} Pessoa;
+
+typedef struct
+{
+    int codigo;
+    int dia, mes, ano;
+    float quilos, altura;
+} Historico_Pesos;
+
 int incluirpessoa(void)
 {
-    typedef struct
-    {
-        int codigo;
-        char nome[20];
-        float altura;
-        float peso;
-    } Pessoa;
 
     Pessoa d;
 
@@ -46,16 +54,23 @@ int mostrarpessoa()
         perror("Erro!!!");
         return EXIT_FAILURE;
     }
+
+    char linha[200];
+    printf("\n--- Pessoas Cadastradas ---\n");
+    printf("Codigo | Nome | Altura | Peso\n");
+    printf("---------------------------\n");
+
+    while (fgets(linha, 200, fp) != NULL)
+    {
+        printf("%s", linha);
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
 }
 
 int incluirpeso(void)
 {
-    typedef struct
-    {
-        int codigo;
-        int dia, mes, ano;
-        float quilos, altura;
-    } Historico_Pesos;
 
     Historico_Pesos a;
 
@@ -80,9 +95,83 @@ int incluirpeso(void)
     return EXIT_SUCCESS;
 }
 
+int mostrarimc(void)
+{
+    FILE *fp;
+    FILE *fp2;
+    fp = fopen("Pessoa.csv", "r");
+    fp2 = fopen("Pessoa.csv", "r");
+    if (fp == NULL || fp2 == NULL)
+    {
+        perror("Erro!!!");
+        return EXIT_FAILURE;
+    }
+
+    char IMC[200];
+    printf("\n--- IMC de Pessoas Cadastradas ---\n");
+    printf("Codigo | IMC\n");
+    printf("---------------------------\n");
+
+    while (fgets(IMC, 200, fp) != NULL)
+    {
+        printf("%s", IMC);
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+
 int main(void)
 {
+    int selecao;
 
+    do
+    {
+        printf("\n--- MENU ---\n");
+        printf("1. Incluir Pessoa\n");
+        printf("2. Mostrar Pessoas cadastradas\n");
+        printf("3. Incluir Historico de pesos\n");
+        printf("4. Mostrar historico de IMCs (NAO IMPLEMENTADO)\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
 
+        if (scanf("%d", &selecao) != 1)
+        {
+            printf("Opcao invalida. Tente novamente.\n");
 
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF)
+                ;
+            selecao = -1;
+            continue;
+        }
+
+        switch (selecao)
+        {
+        case 1:
+            incluirpessoa();
+            break;
+
+        case 2:
+            mostrarpessoa();
+            break;
+
+        case 3:
+            incluirpeso();
+            break;
+        case 4:
+            mostrarimc;
+            break;
+        case 0:
+            printf("Saindo...");
+            break;
+
+        default:
+            printf("Opcao invalida, escolha de novo");
+            break;
+        }
+
+    } while (selecao != 0);
+
+    return 0;
 }
